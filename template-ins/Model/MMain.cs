79793392a8ef27a43b339148394da2443e91c23b
ZhouYiDuo <896894913@qq.com>
2019-01-21 20:11:36 +0800

    上传模板
    WPF MVVM

---
+++ template-ins/Model/MMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template_ins.Model
+{
+    class MMain
+    {
+    }
+}
---
+++ template-ins/ViewModel/VmMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template_ins.ViewModel
+{
+    public class VmMain : NotificationObject
+    {
+        public VmMain()
+        {
+            _text = "Hello Text";
+        }
+
+        private string _text;
+        public string Text
+        {
+            set
+            {
+                _text = value;
+                this.RaisePropertyChanged("Text");
+            }
+            get
+            {
+                return _text;
+            }
+        }
+    }
+}
